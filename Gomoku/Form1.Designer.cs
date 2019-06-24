8bbf469d2220208b88329c83ddaa64d31b734c26
Xskullibur <simasas2222@gmail.com>
2019-06-24 18:23:02 +0800

    Add project files.

---
+++ Gomoku/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Gomoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(70, 29);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.resetToolStripMenuItem.Text = "&Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gamePanel);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(832, 604);
+            this.panel1.TabIndex = 2;
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.AutoScroll = true;
+            this.gamePanel.BackColor = System.Drawing.SystemColors.Info;
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(601, 604);
+            this.gamePanel.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(601, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(231, 604);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 637);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Gomoku/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class Form1 : Form
+    {
+        string IMAGE_PATH = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()) + "\\img";
+
+        const int BOARD_WIDTH = 15;
+        const int BOARD_HEIGHT = 15;
+
+        const string PLAYER_DIE = "black_die.png";
+        const string PLAYER_SYMBOL = "o";
+
+        const string COMPUTER_DIE = "white.die.png";
+        const string COMPUTER_SYMBOL = "x";
+
+        string[,] GAME_BOARD = new string[BOARD_WIDTH, BOARD_HEIGHT];
+
+
+        public bool winCondition(string symbol)
+        {
+
+            /*****      Check Horizontal Victory        *****/
+            for (int row = 0; row < BOARD_HEIGHT; row++)        // All Rows
+            {
+                for (int col = 0; col < BOARD_WIDTH-4; col++)   // First 6 col (Prevent Crash From col 7 onwards)
+                {
+
+                    if (GAME_BOARD[row, col] == symbol)         // Check for symbol
+                    {
+
+                        int diesInRow = 0;
+
+                        for (int i = 1; i < 5; i++)             // Check Next 4 Col
+                        {           
+                            if (GAME_BOARD[row, col] == GAME_BOARD[row, col + i])
+                            {
+                                diesInRow++;                    // Count the number of times in a row
+                            }
+                            else                               
+                            {
+                                break;                          // Stop the loop if not in a row
+                            }
+                        }
+
+                        if (diesInRow == 4)
+                        {
+                            return true;
+                        }
+
+                    }
+
+                }
+            }
+
+            /*****      Check Vertical Victory          *****/
+            for (int col = 0; col < BOARD_WIDTH; col++)         // All Cols
+            {
+                for (int row = 0; row < BOARD_HEIGHT-4; row++)  // First 6 Rows (Prevent Crash at row 7 onwards)
+                {
+
+                    if (GAME_BOARD[row, col] == symbol)         // Check Symbol
+                    {
+
+                        int diesInRow = 0;
+
+                        for (int i = 1; i < 5; i++)             // Check next 4 rows
+                        {
+                            if (GAME_BOARD[row, col] == GAME_BOARD[row + i, col])
+                            {
+                                diesInRow++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (diesInRow == 4)
+                        {
+                            return true;
+                        }
+
+                    }
+
+                }
+            }
+
+            /*****      Check Top-Left Diagonal Victory          *****/
+            for (int col = 0; col < BOARD_WIDTH-4; col++)
+            {
+                for (int row = 0; row < BOARD_HEIGHT-4; row++)
+                {
+
+                    if (GAME_BOARD[row, col] == symbol)
+                    {
+
+                        int diesInRow = 0;
+
+                        for (int i = 1; i < 5; i++)
+                        {
+                            if (GAME_BOARD[row, col] == GAME_BOARD[row + i, col + i])
+                            {
+                                diesInRow++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (diesInRow == 4)
+                        {
+                            return true;
+                        }
+
+                    }
+
+                }
+            }
+
+            /*****      Check Bottom-Right Diagonal Victory          *****/
+            for (int row = BOARD_HEIGHT-1; row > 4; row--)
+            {
+                for (int col = 0; col < BOARD_WIDTH-4; col++)
+                {
+                    if (GAME_BOARD[row, col] == symbol)
+                    {
+
+                        int diesInRow = 0;
+
+                        for (int i = 1; i < 5; i++)
+                        {
+                            if (GAME_BOARD[row, col] == GAME_BOARD[row - i, col + i])
+                            {
+                                diesInRow++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (diesInRow == 4)
+                        {
+                            return true;
+                        }
+
+                    }
+                }
+            }
+
+            return false; 
+        }
+
+        public void generateBoard()
+        {
+            int startX = 10;
+            int startY = 10;
+            int btnHeight = 35;
+            int btnWidth = 35;
+            int dist = 10;
+
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                for (int y = 0; y < BOARD_HEIGHT; y++)
+                {
+                    Button btn = new Button();
+                    btn.BackColor = Color.SandyBrown;
+                    btn.Name = $"{x}-{y}_btn";
+                    btn.TabStop = false;
+                    btn.Click += button_click;
+
+                    btn.Top = startX + (x * btnHeight + dist);
+                    btn.Left = startY + (y * btnWidth + dist);
+                    btn.Width = btnWidth;
+                    btn.Height = btnHeight;
+
+                    gamePanel.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            //Set Gameboard
+            string btnPosition = btn.Name.Split('_')[0];
+            int row = Convert.ToInt32(btnPosition.Split('-')[0]);
+            int column = Convert.ToInt32(btnPosition.Split('-')[1]);
+
+            //Update Gameboard
+            GAME_BOARD[row, column] = PLAYER_SYMBOL;
+
+            //Set PLAYER DIE
+            btn.BackgroundImage = Image.FromFile($"{IMAGE_PATH}\\{PLAYER_DIE}");
+            btn.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //Disable Btn
+            btn.Enabled = false;
+
+            if (winCondition(PLAYER_SYMBOL))
+            {
+                MessageBox.Show("Player Wins!");
+            }
+        }
+
+        private void reset_board()
+        {
+            foreach (Control control in gamePanel.Controls)
+            {
+                control.BackgroundImage = null;
+                control.Enabled = true;
+            }
+
+            for (int col = 0; col < BOARD_WIDTH-1; col++)
+            {
+                for (int row = 0; row < BOARD_HEIGHT-1; row++)
+                {
+                    GAME_BOARD[row, col] = "";
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            generateBoard();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            reset_board();
+        }
+    }
+}
